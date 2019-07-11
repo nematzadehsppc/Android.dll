f9d78b8933a962410c898458f31a6be3f3b1f119
Behzad Nematzadeh <b_nematzadeh@sppc.co.ir>
2019-07-11 17:14:34 +0430

    add sync

---
+++ Service/Sync/Sync.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using TadbirModels;
+
+namespace Tadbir
+{
+    public class Sync : CodeActivity<FileStream>
+    {
+        // =====================================================
+        List<int[]> customerIdList = new List<int[]>();
+        List<Customer> customerList = new List<Customer>();
+
+        // =====================================================
+        public string ip { get; set; }
+
+        public int port { get; set; }
+
+        public string database { get; set; }
+
+        public string version { get; set; }
+
+        public int userId { get; set; }
+
+        public int fpId { get; set; }
+
+        public string serviceKey { get; set; }
+
+        // =====================================================
+
+        //public InArgument<string> IP { get; set; }
+
+        //public InArgument<int> Port { get; set; }
+
+        //public InArgument<string> Database { get; set; }
+
+        //public InArgument<string> Version { get; set; }
+
+        //public InArgument<int> UserId { get; set; }
+
+        //public InArgument<int> FPId { get; set; }
+
+        //public InArgument<string> ServiceKey { get; set; }
+
+        // =====================================================
+
+        protected override FileStream Execute(CodeActivityContext context)
+        {
+            ip = "130.185.76.7";
+            port = 9010;
+            database = "SARDOMID";
+            version = "V3.1";
+            userId = 9;
+            fpId = 3;
+            serviceKey = "1234";
+
+            var customer = getAllCustomer(fpId, userId);
+            customerList.AddRange(customer);
+
+            string jsondata = new JavaScriptSerializer().Serialize(customerList);
+            File.WriteAllText(System.Web.HttpContext.Current.Server.MapPath("~/JsonData/jsondata.txt"), jsondata);
+            return new FileStream(System.Web.HttpContext.Current.Server.MapPath("~/JsonData/jsondata.txt"), FileMode.Create);
+        }
+
+        private List<Customer> getAllCustomer(int fpid, int userId)
+        {
+            string url = string.Format("Inventory/Customer/GetAllCustomer?fpid={0}&userId={1}&key={2}", userId, fpId, serviceKey);
+            return GetListAsyncTask<Customer>(url);
+        }
+
+        private List<T> GetListAsyncTask<T>(string uri)
+        {
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            string baseAddress = string.Format("http://{0}:{1}/{2}/{3}/", ip, port, database, version);
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseAddress);
+
+            Task<HttpResponseMessage> responseMessage = client.GetAsync(uri);
+            responseMessage.Wait();
+
+            Task<string> task = responseMessage.Result.Content.ReadAsStringAsync();
+            task.Wait();
+
+            var content = task.Result;
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            var obj = serializer.Deserialize<List<T>>(content);
+            return obj;
+        }
+    }
+}
---
+++ SyncTable/SyncForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SyncTable
+{
+    partial class SyncForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_request = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_request
+            // 
+            this.btn_request.Location = new System.Drawing.Point(54, 12);
+            this.btn_request.Name = "btn_request";
+            this.btn_request.Size = new System.Drawing.Size(75, 23);
+            this.btn_request.TabIndex = 0;
+            this.btn_request.Text = "Request";
+            this.btn_request.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 50);
+            this.Controls.Add(this.btn_request);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_request;
+    }
+}
+
